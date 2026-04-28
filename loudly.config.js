12c3726c3e8a2624f6c0357b02e4bfa161c5b986
Erno Vuori <erno.vuori@gmail.com>
2026-04-28 16:16:49 +0300

    chore: add configuration file

---
+++ loudly.config.js
@@ -0,0 +1,14 @@
+/** @type {import('./src/types/config').LoudlyConfig} */
+export default {
+  content: {
+    repo: "evuori/personal-llm-wiki",
+    branch: "public",
+    path: "/content",
+  },
+  site: {
+    title: "Erno Vuori",
+    description: "Your description",
+    url: "https://ernovuori.com",
+    defaultOgImage: "/og-default.png",
+  },
+};
